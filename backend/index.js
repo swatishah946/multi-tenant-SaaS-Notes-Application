e78a372e217c52065472c1bbf1f8c3efe99f6060
swatishah946 <shahswati946@gmail.com>
2025-09-15 23:57:12 +0530

    updated cord in backend after deployment

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,24 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+import cors from "cors";
+
+const allowedOrigins = [
+  "https://multi-tenant-saas-frontend.vercel.app/",  
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+   
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  allowedHeaders: ["Content-Type", "Authorization", "x-tenant"],
+}));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=>console.log("MongoDB connected!"))
